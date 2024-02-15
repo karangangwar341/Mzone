128842ff7507dc0e54998f7508954068a6d80c5d
Aditya210703 <vermaaditya2356@gmail.com>
2024-02-15 19:25:48 +0530

    Added player

--- vision/src/components/Player.js
+++ vision/src/components/Player.js
@@ -1,6 +1,10 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { database } from '.././firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
-const Player = ({ audios }) => {
+const userRef = collection(database, 'musicdata');
+
+const Player = () => {
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
@@ -8,11 +12,32 @@ const Player = ({ audios }) => {
   const [isMuted, setIsMuted] = useState(false);
   const [isLooping, setIsLooping] = useState(false);
   const audioRef = useRef();
+  //
+  const [musicData, setMusicData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(userRef);
+        const data = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setMusicData(data);
+      } catch (error) {
+        console.error('Error getting documents: ', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  //
+  console.log(musicData);
 
   const nextSong = () => {
     setCurrentSongIndex((prevIndex) => {
       let nextIndex = prevIndex + 1;
-      if (nextIndex >= audios.length) nextIndex = 0;
+      if (nextIndex >= musicData.length) nextIndex = 0;
       return nextIndex;
     });
   };
@@ -20,7 +45,7 @@ const Player = ({ audios }) => {
   const prevSong = () => {
     setCurrentSongIndex((prevIndex) => {
       let nextIndex = prevIndex - 1;
-      if (nextIndex < 0) nextIndex = audios.length - 1;
+      if (nextIndex < 0) nextIndex = musicData.length - 1;
       return nextIndex;
     });
   };
@@ -64,7 +89,7 @@ const Player = ({ audios }) => {
   };
 
   useEffect(() => {
-    audioRef.current.src = audios[currentSongIndex].src;
+    audioRef.current.src = musicData[currentSongIndex]?.songlink;
     if (isPlaying) audioRef.current.play();
     audioRef.current.addEventListener('ended', handleEnded);
     return () => {
@@ -96,10 +121,10 @@ const Player = ({ audios }) => {
         className="slider w-full "
       />
 
-      <p>{audios[currentSongIndex].artist} - {audios[currentSongIndex].title}</p>
+      <p>{musicData[currentSongIndex]?.artist} - {musicData[currentSongIndex]?.title}</p>
 
       <div className="flex justify-between">
-        <img src={audios[currentSongIndex].imgurl} className='w-12 h-12 rounded-2xl'/>
+        <img src={musicData[currentSongIndex]?.thumbnail} className='w-12 h-12 rounded-2xl'/>
         <div className='py-2'>
           <button
             className="border-2 border-white/60 px-3 py-1 mx-1 hover:bg-white/10 rounded-3xl"
